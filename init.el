511f643e72466ed46fb39414af031bd4762e7089
Alan Patterson <alan@MacBook-Pro.home>
2017-09-30 13:42:17 +0100

    updated loading of clang-format

--- init.el
+++ init.el
@@ -75,7 +75,6 @@
 (setq clang-format-style-option "Google")
 
 (use-package clang-format
-  :defer t
   :config (add-hook 'before-save-hook
                     (lambda ()
                         (when (eq major-mode 'c++-mode)
